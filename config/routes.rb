b28b4ec2efbb636906ebdf5b45da89e89f27a7dd
Sergio Santiago <sergio@secture.com>
2022-05-03 12:53:58 +0200

    Basic CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles
 
   # Defines the root path route ("/")
   root 'articles#index'
-
-  get 'articles', to: 'articles#index'
 end
