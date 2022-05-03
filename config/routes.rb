ae7df1e6f68d7d7e5530b388a31a821e9c210d0b
Sergio Santiago <sergio@secture.com>
2022-05-03 19:21:14 +0200

    Related entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # Defines the root path route ("/")
   root 'articles#index'
